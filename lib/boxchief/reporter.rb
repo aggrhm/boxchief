3bde865cb42bcee723e4df427a251e48bf7d9fff
Alan Graham <alan@productlab.com>
2014-08-21 16:18:45 -0400

    updated reporter to run as process

--- lib/boxchief/reporter.rb
+++ lib/boxchief/reporter.rb
@@ -4,24 +4,57 @@ module Boxchief
 
   class Reporter
 
-    def initialize(opts)
+    def initialize(opts = {})
       @options = opts
-      @app_token = opts[:app_token]
-      @url = opts[:url] || "http://boxchief.com"
+      @options[:interval] = 60
+      @logger = nil
+      self.process_options
+    end
 
+    def process_options
+
+    end
+
+    def run
+      # ensure options
       if @options[:container].nil? && @options[:server].nil? && @options[:host].nil?
         @options[:server] = Boxchief::Utils.get_hostname
       end
+
+      # logger
+      @logger = Logger.new( (@options[:log_path] || '/var/log/reporter.log'), 1, 1024*1024)
+      @logger.info "Running with options: #{@options.inspect}"
+
+      # loop
+      loop do
+        data = build_data
+        @logger.info data.inspect
+        if @options[:app_token] && data
+          self.report(data)
+        end
+        sleep @options[:interval]
+      end
+    end
+
+    def build_data
+
     end
 
     def report(data)
+      url = @options[:url] || "http://boxchief.com"
       rep = {}.merge(@options)
-      rep['app_token'] = @app_token
       rep['data'] = data.to_json
-      conn = Faraday.new(:url => @url)
+      conn = Faraday.new(:url => url)
       resp = conn.post "/api/stats/report", rep
       return resp
     end
+
+    def ensure_rails_loaded
+      unless defined?(Rails)
+        require File.join(@options[:app_path], 'config', 'environment')
+      end
+    end
+    
   end
 
 end
